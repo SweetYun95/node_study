1db28fc0f52079bc67e689e8e75b079219dc6a4f
SweetYun95 <isr01154@naver.com>
2025-07-03 11:39:20 +0900

    2507031139

--- ch03/02_Middleware/globalMiddleware.js
+++ ch03/02_Middleware/globalMiddleware.js
@@ -1,15 +1,16 @@
 const express = require('express')
-require('dotenv').config() 
+require('dotenv').config()
 
 const app = express()
 app.set('port', process.env.PORT || 3000)
 
+app.get('/', (req, res) => {
+   res.send(`홈페이지`)
+})
 
-
-
-
-
-
+app.get('/about', (req, res) => {
+   res.send(`소개 페이지`)
+})
 
 app.listen(app.get('port'), () => {
    console.log(`서버가 작동 중 입니다. http://localhost:${app.get('port')}`)
